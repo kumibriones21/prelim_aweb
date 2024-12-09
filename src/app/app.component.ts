2a2cfce76a2029f330f024ffd00b5874ae1206ba
kumibriones21 <kumibrionesschool@gmail.com>
2024-12-09 12:06:34 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { FeaturedComponent } from "./components/featured/featured.component";
+import { GalleryComponent } from "./components/gallery/gallery.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FeaturedComponent, GalleryComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
